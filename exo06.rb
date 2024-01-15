1be7683295a1aa958b47588ca173971e565d3e61
splmloic <l2ct@protonmail.com>
2024-01-15 14:57:30 +0100

    on déclare des variables puis ont fait un calcul isolé avec elles

---
+++ exo06.rb
@@ -0,0 +1,6 @@
+#On commence par déclarer les variables, puis on fait le calcul du nombre d'heure travailler au global sur thp (soit 10*5*11)
+number_of_hours_worked_per_day = 10
+number_of_days_worked_per_week = 5
+number_of_weeks_in_THP = 11
+
+puts "Travail : #{number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
