dbb13542ba96da89b3cbfec98bf7dff38108828d
splmloic <l2ct@protonmail.com>
2024-01-15 15:40:05 +0100

    j'ai simplifié mon code pour tout avoir sur la meme ligne

--- exo10.rb
+++ exo10.rb
@@ -2,5 +2,4 @@ puts "tu es née en quelle année ?"
 print "> "
 Year = gets.chomp.to_i
 
-puts "En l'an de grace 2017 tu avais"
-print 2017 - Year
+puts "En l'an de grace 2017 tu avais #{2017 - Year} ans"
